27de643534afbc2d4af4aa2eafee8ba7655170cb
jakobakoba <jakobakoba@gmail.com>
2023-03-16 18:17:11 +0600

    created composable fun Button

---
+++ app/src/main/java/com/bor96dev/calculator/Button.kt
@@ -0,0 +1,35 @@
+package com.bor96dev.calculator
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Button(
+    symbol: String,
+    modifier: Modifier,
+    onClick: () -> Unit,
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .clip(RoundedCornerShape(20.dp))
+            .clickable { onClick() }
+            .then(modifier)
+    ) {
+        Text(
+            text = symbol,
+            color = MaterialTheme.colors.primary,
+            fontSize = 25.sp,
+        )
+    }
+
+}
--- app/src/main/java/com/bor96dev/calculator/MainActivity.kt
+++ app/src/main/java/com/bor96dev/calculator/MainActivity.kt
@@ -3,41 +3,15 @@ package com.bor96dev.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.bor96dev.calculator.ui.theme.CalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
+        setContent{
+
         }
+
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CalculatorTheme {
-        Greeting("Android")
-    }
-}
